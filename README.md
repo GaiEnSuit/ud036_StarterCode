c224f16bea38a8324c1a68f96572794522a08ef7
K Truong <truong.k77@gmail.com>
2017-09-07 18:00:55 +0000

    create meadia.py and entertainment_center.py

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# ud036_StarterCode
-Source code for a Movie Trailer website.
+Back end code written for the fresh_tomatoes.py
+
+Run entertanment_center.py as it contains teh method to create the html file.
---
+++ entertainment_center.py
@@ -0,0 +1,12 @@
+import media
+import fresh_tomatoes
+
+moviesList = []
+
+movie1 = media.Movie("Perfect Blue", "https://www.youtube.com/watch?v=cwGMDibs_KY", "https://upload.wikimedia.org/wikipedia/en/b/b2/PerfectBlue.jpg")
+movie2 = media.Movie("Pirates of the Caribbean", "https://www.youtube.com/watch?v=XibzC-e_s5M", "http://thumbs1.ebaystatic.com/d/l225/m/mAcgK6wxgKcw1bJ3pGIc6EQ.jpg" )
+
+moviesList.append(movie1)
+moviesList.append(movie2)
+
+fresh_tomatoes.open_movies_page(moviesList)
